95c8ac64bf180315d52c444cbbcdc584c6b2193b
SarangChopra123 <sarang1234chopra@gmail.com>
2025-06-29 21:08:22 +0530

    Create set1_basics -->day1.sql
    feeling great to ' learn in public '

---
+++ set1_basics -->day1.sql
@@ -0,0 +1,22 @@
+---day 1 : SELECT + WHERE 
+select * from employees where department = 'HR';
+
+select name, salary from employees where salary > 55000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+added day 1 queries...
